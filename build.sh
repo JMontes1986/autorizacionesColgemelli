0e9df24747cfd551d93dd7e2cadb638fd8824a54
JMontes1986 <damon.1986@icloud.com>
2025-06-25 08:49:16 -0500

    build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ set -euo pipefail
 
 cat > env.js <<EOL
 window.process = { env: {
-  SUPABASE_URL: "${SUPABASE_URL}",
-  SUPABASE_ANON_KEY: "${SUPABASE_ANON_KEY}"
+  SUPABASE_URL: "${https://mbosvnmhnbrslfwlfcxu.supabase.co}",
+  SUPABASE_ANON_KEY: "${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ib3N2bm1obmJyc2xmd2xmY3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1ODU2MzUsImV4cCI6MjA2NTE2MTYzNX0.evgs5gWsCRyfeo273tLiAAoIdB-IjMaPq8U23xK4lqc}"
 }};
 EOL
 
