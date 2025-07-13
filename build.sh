7660e7e18f1cc03649aebc9d8ff1613d469bf1fa
JMontes <damon.1986@icloud.com>
2025-07-13 15:01:29 -0500

    build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-"${SUPABASE_URL:?SUPABASE_URL is required}"
-: "${SUPABASE_ANON_KEY:?SUPABASE_ANON_KEY is required}"
+: "${https://mbosvnmhnbrslfwlfcxu.supabase.co}"
+: "${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ib3N2bm1obmJyc2xmd2xmY3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1ODU2MzUsImV4cCI6MjA2NTE2MTYzNX0.evgs5gWsCRyfeo273tLiAAoIdB-IjMaPq8U23xK4lqc}"
 
 cat > env.js <<EOL
 window.process = {
