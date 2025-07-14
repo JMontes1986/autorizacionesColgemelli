769cb87c195da8f5ef3a2587ed93bddca598e730
JMontes <damon.1986@icloud.com>
2025-07-13 21:04:46 -0500

    functions/verifyCsrf.js

--- functions/verifyCsrf.js
+++
@@ -1,8 +0,0 @@
-module.exports = function verifyCsrf(req, res, next) {
-  const expected = process.env.CSRF_SECRET;
-  const token = req.get('X-CSRF-Token');
-  if (!token || token !== expected) {
-    return res.status(403).json({ error: 'Invalid CSRF token' });
-  }
-  next();
-};
